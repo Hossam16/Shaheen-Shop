bca3ed544bb42c3bb522d867a6be8310957bc4ba
Hossam16 <hossamalaa161998@gmail.com>
2021-07-05 15:15:23 +0200

    Free Shipping fied

--- GetData/GetFreeDel.php
+++ GetData/GetFreeDel.php
@@ -3,7 +3,7 @@ include '../config.php';
 
 
 $connection=new config();
-$Sql="SELECT * FROM products WHERE Size='Free' AND Stock!=0 ORDER BY RAND() LIMIT 10";
+$Sql="SELECT * FROM products WHERE Size='Free' AND Stock!=0 AND SID!=2 ORDER BY RAND() LIMIT 10";
 $Queryresult=$connection->datacon()->query($Sql);
 $Result=array();
 
